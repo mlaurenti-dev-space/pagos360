d16e7f85dc333d9a8d315fbfc9a41305d2b6c890
Mariano Laurenti <mla@qubikcommerce.com>
2025-07-11 18:19:16 -0300

    Adds initial project setup with Maven Wrapper
    Sets up a Spring Boot project with Kotlin, R2DBC, and Webflux.
    
    Configures Maven Wrapper for consistent builds and dependency management.
    
    Includes necessary dependencies for reactive programming and database connectivity with PostgreSQL.
    
    Sets up basic .gitignore and .gitattributes files.

---
+++ src/main/kotlin/com/devspace/pagos360/Pagos360Application.kt
@@ -0,0 +1,11 @@
+package com.devspace.pagos360
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Pagos360Application
+
+fun main(args: Array<String>) {
+    runApplication<Pagos360Application>(*args)
+}
---
+++ src/test/kotlin/com/devspace/pagos360/Pagos360ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devspace.pagos360
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Pagos360ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
