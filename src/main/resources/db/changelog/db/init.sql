3b0f0ff9c7afca36c89e2079078beec2d7217c5c
Mariano Laurenti <mla@qubikcommerce.com>
2025-07-15 19:04:45 -0300

    Adds default UUID generation for payment requests
    Ensures that each payment request automatically receives a unique identifier upon creation.
    
    This simplifies the process of creating new payment requests and avoids potential issues with missing or duplicate IDs.

--- src/main/resources/db/changelog/db/init.sql
+++ src/main/resources/db/changelog/db/init.sql
@@ -3,7 +3,7 @@ EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE pay_payment_requests
 (
-    id             UUID PRIMARY KEY,
+    id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     description    VARCHAR(255)   NOT NULL,
     first_due_date TIMESTAMP      NOT NULL,
     first_total    NUMERIC(15, 2) NOT NULL,
